955710e9b160d245104381bababe18201abb4c0a
Davy Biesemans <r0982281@student.thomasmore.be>
2025-02-15 13:57:48 +0100

    Initial Commit

---
+++ src/main/java/be/thomasmore/projectsocialmedia/ProjectSocialMediaApplication.java
@@ -0,0 +1,13 @@
+package be.thomasmore.projectsocialmedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSocialMediaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectSocialMediaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/thomasmore/projectsocialmedia/ProjectSocialMediaApplicationTests.java
@@ -0,0 +1,13 @@
+package be.thomasmore.projectsocialmedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSocialMediaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
