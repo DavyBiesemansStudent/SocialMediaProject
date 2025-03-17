cc1887680a742e43f56e7010c94b830af80b7670
Davy Biesemans <r0982281@student.thomasmore.be>
2025-03-17 15:20:20 +0100

    users can now register a new account.

--- src/main/java/be/thomasmore/projectsocialmedia/controllers/UserController.java
+++ src/main/java/be/thomasmore/projectsocialmedia/controllers/UserController.java
@@ -1,13 +1,33 @@
 package be.thomasmore.projectsocialmedia.controllers;
+import be.thomasmore.projectsocialmedia.model.AppUser;
+import be.thomasmore.projectsocialmedia.repositories.AppUserRepository;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import java.security.Principal;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
+
+    @Autowired
+    AppUserRepository appUserRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private JdbcUserDetailsManager userDetailsManager;
 
     @GetMapping("/login")
     public String login(Model model, Principal principal) {
@@ -24,5 +44,47 @@ public class UserController {
             return "redirect:/feed";
         }
         return "user/logout";
+    }
+
+    @GetMapping("/register")
+    public String register(Principal principal) {
+        if (principal != null) {
+            return "redirect:/feed";
+        }
+        return "user/register";
+    }
+
+    @PostMapping("/register")
+    public String registerPost(Principal principal,
+                               HttpServletRequest request,
+                               @RequestParam String username,
+                               @RequestParam String password,
+                               @RequestParam String name,
+                               @RequestParam String profilePictureUrl,
+                               @RequestParam String bio,
+                               Model model) throws ServletException {
+        if (principal != null) return "redirect:/feed";
+        if (userDetailsManager.userExists(username)){
+            model.addAttribute("error", "Username is already taken");
+            return "user/register";
+        }
+
+        UserDetails user = org.springframework.security.core.userdetails.User
+                .withUsername(username)
+                .password(passwordEncoder.encode(password))
+                .roles("USER")
+                .build();
+        userDetailsManager.createUser(user);
+
+        AppUser newAppUser = new AppUser();
+        newAppUser.setName(name);
+        newAppUser.setUsername(username);
+        newAppUser.setProfilePictureUrl(profilePictureUrl);
+        newAppUser.setBio(bio);
+        appUserRepository.save(newAppUser);
+
+        request.login(username, password);
+
+        return "redirect:/feed";
     }
 }
